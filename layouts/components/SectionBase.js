9ad9fb1d368b1be4d9ace157182c053fd3434ae5
Felipe Vargas <felipe.vargas@gmail.com>
2025-02-07 21:20:39 -0500

    mt-5

--- layouts/components/SectionBase.js
+++ layouts/components/SectionBase.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionBase = ({ children }) => {
   return (
-    <section className="flex items-center justify-center min-h-screen bg-white py-12 px-4 sm:px-6 lg:px-8">
+    <section className="flex items-center justify-center min-h-screen bg-white py-12 px-4 sm:px-6 lg:px-8 mt-5">
       <div className="w-full space-y-8">
         {children}
       </div>
